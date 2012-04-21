32f6a51c1accdce80e6380ff6399cf078daad7c5
Richie McMahon <rmcmahon@adobe.com>
2012-04-21 19:53:57 +0100

    Updated rake tasks and gemspec

--- Rakefile
+++ Rakefile
@@ -1,2 +1,20 @@
 #!/usr/bin/env rake
+require 'rdoc/task'
 require "bundler/gem_tasks"
+
+desc "Build the RDoc API documentation"
+RDoc::Task.new do |rdoc|
+  rdoc.rdoc_dir = "doc"
+  rdoc.title    = "Capistrano S3 Copy"
+  rdoc.options += %w(--main README)
+  rdoc.rdoc_files.include('README.md', 'LICENSE', 'lib/**/*.rb')
+end
+
+desc "Build documentation"
+task :doc => [ :rdoc ]
+
+desc "Clean up generated directories and files"
+task :clean do
+  rm_rf "pkg"
+  rm_rf "doc"
+end
--- lib/capistrano/recipes/deploy/strategy/s3_copy.rb
+++ lib/capistrano/recipes/deploy/strategy/s3_copy.rb
@@ -1,17 +1,57 @@
 require 'capistrano/recipes/deploy/strategy/copy'
-require 'fileutils'
-require 'tempfile'  # Dir.tmpdir
 
 module Capistrano
   module Deploy
     module Strategy
       class S3Copy < Copy
-          # Distributes the file to the remote servers
-          def distribute!
-            raise "uhuihuihuhi"
-#            upload(filename, remote_filename)
-#            run "cd #{configuration[:releases_path]} && #{decompress(remote_filename).join(" ")} && rm #{remote_filename}"
+      
+        S3CMD_VARS = ["aws_access_key_id", "aws_secret_access_key"]
+      
+        def initialize(config={})
+          super(config)          
+          
+          s3cmd_vars = []
+          S3CMD_VARS.each do |var|
+            value = configuration[var.to_sym]
+            raise Capistrano::Error, "Missing configuration[:#{var}] setting" if value.nil?
+            s3cmd_vars << "#{var.upcase}=#{value}"
+          end       
+          @aws_environment = s3cmd_vars.join(" ")                 
+          
+          @bucket_name = configuration[:aws_s3_copy_bucket]
+          raise Capistrano::Error, "Missing configuration[:aws_s3_copy_bucket]" if @bucket_name.nil?
+        end      
+        
+        def check!
+          super.check do |d|
+            d.local.command("s3cmd")          
+            d.remote.command("s3cmd")
           end
+        end          
+      
+        # Distributes the file to the remote servers          
+        def distribute!
+          package_path = filename
+          package_name = File.basename(package_path)
+          s3_package = "s3://#{bucket_name}/#{rails_env}/#{package_name}"
+          system("#{aws_environment} s3cmd put #{package_path} #{s3_package} 2>&1")
+          
+          if $? != 0
+            raise Capistrano::Error, "shell command failed with return code #{$?}"
+          end          
+          
+          run "#{aws_environment} s3cmd get #{bucket_name}:#{rails_env}/#{package_name} #{remote_filename}"
+          run "mkdir #{configuration[:release_path]} && cd #{configuration[:release_path]} && #{decompress(remote_filename).join(" ")} && rm #{remote_filename}"
+          logger.debug "done!"
+        end
+          
+        def aws_environment            
+          @aws_environment
+        end
+        
+        def bucket_name
+          @bucket_name
+        end                  
       end
     end
   end
